1969226e9d7608a50926e7262b183567d95587ec
Sergii Lutsanych <s.lutsanych@datacapitalmanagement.com>
2018-10-17 18:05:39 -0300

    class_index is renamed to class_label

--- ensembling/stacking/stacker.py
+++ ensembling/stacking/stacker.py
@@ -8,7 +8,7 @@ from modeling.prediction import BaseEstimator
 class Stacker(BaseEstimator):
 
     def __init__(self, oof_input_files, train_df, test_df, target_column, index_column, stacker_model,
-                 predict_probability, class_index, eval_metric, metrics_scorer, metrics_decimals=6, target_decimals=6,
+                 predict_probability, class_label, eval_metric, metrics_scorer, metrics_decimals=6, target_decimals=6,
                  cols_to_exclude=[], num_folds=5, stratified=False, kfolds_shuffle=True, cv_verbosity=1000,
                  bagging=False, data_split_seed=789987, model_seeds_list=[27], predict_test=True, project_location='',
                  output_dirname=''):
@@ -22,7 +22,7 @@ class Stacker(BaseEstimator):
                                                          target_column, index_column, target_decimals)
 
         super(Stacker, self).__init__(
-            self.train_oof, self.test_oof, target_column, index_column, stacker_model, predict_probability, class_index,
+            self.train_oof, self.test_oof, target_column, index_column, stacker_model, predict_probability, class_label,
             eval_metric, metrics_scorer, metrics_decimals, target_decimals, cols_to_exclude, num_folds, stratified,
             kfolds_shuffle, cv_verbosity, bagging, data_split_seed, model_seeds_list, predict_test, path_output_dir
         )
@@ -68,7 +68,7 @@ def run_stacker_kaggle_example(stacker_model='logistic_regression', debug=True):
     }
 
     # Parameters
-    class_index = 1  # in Target
+    class_label = 1  # in Target
     project_location = 'c:\Kaggle\home_credit_default_risk'  # ''
     output_dirname = ''  # 'solution'
     target_column = 'TARGET'
@@ -136,7 +136,7 @@ def run_stacker_kaggle_example(stacker_model='logistic_regression', debug=True):
     stacker = Stacker(
         oof_input_files=oof_input_files, train_df=train_data, test_df=test_data, target_column=target_column,
         index_column=index_column, stacker_model=stacker_wrapped, predict_probability=predict_probability,
-        class_index=class_index, eval_metric=eval_metric, metrics_scorer=metrics_scorer,
+        class_label=class_label, eval_metric=eval_metric, metrics_scorer=metrics_scorer,
         metrics_decimals=metrics_decimals, target_decimals=target_decimals, cols_to_exclude=cols_to_exclude,
         num_folds=num_folds, stratified=stratified, kfolds_shuffle=kfolds_shuffle, cv_verbosity=cv_verbosity,
         bagging=bagging, predict_test=predict_test, data_split_seed=data_split_seed, model_seeds_list=model_seeds_list,
