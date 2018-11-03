c2d52fecdd84b2a71f44b8092825502971b46310
Sergii Lutsanych <s.lutsanych@datacapitalmanagement.com>
2018-11-03 17:09:10 -0300

    slight refactoring

--- generic_tools/loggers.py
+++ generic_tools/loggers.py
@@ -1,13 +1,19 @@
 import sys
 import logging
 
+from generic_tools.utils import get_current_timestamp
+
+# Possible values here are DEBUG, INFO, WARN, ERROR and CRITICAL
+LOG_LEVEL = logging.INFO  # logging level
+
 
 def configure_logging():
+    # TODO: to think whether to add or not logging to the file
     # logPath = r'c:\Kaggle\FastMLFramework\examples\classification\multiclass\iris'
-    # fileName = 'demo.log'
+    # fileName = get_current_timestamp() + '.log'
     logging.basicConfig(format='[%(asctime)s] %(levelname)s %(name)s: %(message)s',
                         datefmt='%H:%M:%S',
-                        level=logging.INFO,
+                        level=LOG_LEVEL,
                         handlers=[
                             # logging.FileHandler(filename=os.path.join(logPath, fileName), mode='w'),
                             logging.StreamHandler(stream=sys.stdout)
