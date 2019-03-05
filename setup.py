2d0ba8aa7a58a44cda656d001202ea739beb7cb6
Sergii Lutsanych <s.lutsanych@datacapitalmanagement.com>
2019-03-05 12:57:12 -0500

    updating env setup

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup
 
 requirements = [
-    "pip==18.1",
-    "numpy==1.15.3",
-    "matplotlib==2.2.3",
-    "pandas==0.23.4",
-    "scipy==1.1.0",
-    "scikit-learn==0.20.0",
-    "xgboost==0.80",
-    "lightgbm==2.1.2",
-    "shap==0.24.0",
-    "graphviz==0.8.4",
+    "pip==19.0.3",
+    "numpy==1.16.2",
+    "matplotlib==3.0.3",
+    "pandas==0.24.1",
+    "scipy==1.2.1",
+    "scikit-learn==0.20.3",
+    "xgboost==0.82",
+    "lightgbm==2.2.3",
+    "shap==0.28.5",
+    "graphviz==0.10.1",
     "pdpbox==0.2.0",
-    "pyhocon==0.3.47",
-    "cachetools==2.1.0",
-    "future==0.16.0",
-    "six==1.11.0",
-    "bayesian-optimization==1.0.0",
-    "seaborn",
-    "luigi"
+    "pyhocon==0.3.51",
+    "cachetools==3.1.0",
+    "future==0.17.1",
+    "six==1.12.0",
+    "bayesian-optimization==1.0.1",
+    "seaborn==0.9.0",
+    "luigi==2.8.3"
 ]
 
 
